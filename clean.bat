cbba56fdeff92ff37d71962823516004631d60ce
Bjørn Nørgaard <bjornsnorgaard@gmail.com>
2021-01-12 16:52:51 +0100

    Added initial files

---
+++ clean.bat
@@ -0,0 +1,3 @@
+echo off
+rem the next line deletes only files and NOT directories.
+del "*.xml" del "*.acn" del "*.xdy" del "*.idx" del "*.bcf" del "*.bbl" del "*.blg" del "*.log" del "*.out" del "*.toc" del "*.aux" del "*.lox" del "*.tps" del "*.nlo" del "*.gz"
