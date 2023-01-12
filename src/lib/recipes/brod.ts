f174ce326a9d571855ed2c29dfbd9cf787aacb1b
Bjørn Nørgaard <bjornsnorgaard@gmail.com>
2023-01-12 14:54:05 +0100

    Wording updates in brod.ts

--- src/lib/recipes/brod.ts
+++ src/lib/recipes/brod.ts
@@ -23,8 +23,8 @@ export const brod: Recipe = {
         {title: "Bagetid", desc: "Du tænder ovnen på alt hvad den trække og smider din støbejernsfætter ind. Vi mødes her igen om ~45 min."},
         {title: "Nu kommer svineriet", desc: "Hør nu efter. Det bliver et klisterhelvede. Der skal masser af mel på bordet og vand på fingerne - i den rækkefølge! Den skal ikke æltes, men bare lige foldes nogle gange. Gør det nogle få gange indtil den ligner en pæn dej med en stram overflade."},
         {title: "Brænd nu ikke dine små dumme fingre", desc: "Smid den på et stykke velmelet bagepapir og sprøjt den gavmildt til med vand. Sænk den så forsigtigt ned i din støbejernsgryde (pas nu på pølserne). Sæt låg på og efterlad i ovnen i 20 minutter før næste inspektion."},
-        {title: "Tjekketid", desc: "Kig til brødet. Hvis den ikke er færdig, sættes du den tilbage i ovnen. Hvis du er i tvivl, så spørger du en voksen."},
+        {title: "Tjekketid", desc: "Kig til brødet. Hvis den ikke er færdig, sættes du den tilbage i ovnen. Hvis du er i tvivl, spørger du en voksen."},
         {title: "La' det lig'", desc: "I mindst en time før du fucker med brødet. Alt er spildt hvis du rør den før - det kan ikke diskuteres!"},
-        {title: "Skær den over på midten", desc: "Hvis du har ventet en time, må du skære den over på midten og sende et billede til mor - det er vigtigt. Husk tandsmør, tykke skiver ost og lidt salt - selv tak!"},
+        {title: "Skær den over på midten", desc: "Når du har ventet i en time, må du skære den over på midten og sende et billede til mor - det er vigtigt. Husk tandsmør, ost i tykke skiver og lidt salt - selv tak!"},
     ],
 };
