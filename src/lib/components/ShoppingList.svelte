6d836de978f207f53221e44b769bb9bef0e673d3
Bjørn Nørgaard <bjornsnorgaard@gmail.com>
2023-01-13 00:22:01 +0100

    Animation for ShoppingList.svelte

--- src/lib/components/ShoppingList.svelte
+++ src/lib/components/ShoppingList.svelte
@@ -2,34 +2,59 @@
     import type { Recipe } from "$lib/types/recipe";
     import { writable } from "svelte/store";
     import { onMount } from "svelte";
+    import { flip } from "svelte/animate";
+    import { cubicInOut } from "svelte/easing";
 
     export let recipe: Recipe = {} as Recipe;
 
     const have = writable<string[]>([]);
 
     onMount(() => {
-        const stored: string[] = JSON.parse(localStorage.getItem(recipe.slug)) || [];
-        have.set(stored);
+        have.set(JSON.parse(localStorage.getItem(recipe.slug)) || []);
+        setTimeout(() => sortIngredients(), 500);
     })
 
-    const toggleIngredient = (name: string) => {
+    const onCheckChange = (name: string) => {
+        toggle(name);
+        sortIngredients();
+        save();
+    };
+
+    function toggle(name: string) {
         if ($have.includes(name)) {
             have.update(list => [...list.filter(i => i != name)])
         } else {
             have.update(list => [...list, name])
         }
+    }
+
+    function sortIngredients() {
+        const h = recipe.ingredients.filter(i => $have.includes(i.name));
+        const n = recipe.ingredients.filter(i => !$have.includes(i.name));
+        recipe.ingredients = [...n, ...h];
+    }
+
+    function save() {
         localStorage.setItem(recipe.slug, JSON.stringify($have))
-    };
+    }
 </script>
 
 <h2>Indkøbslisten</h2>
 
 <div class="gap-4 rounded-lg p-4 drop-shadow-lg grid-list bg-neutral text-neutral-content">
-    {#each recipe.ingredients as i (i)}
-        <div class="flex justify-between gap-4">
-            <input type="checkbox" checked={$have.includes(i.name) ? "checked" : ""}
-                   on:click={() => toggleIngredient(i.name)} class="checkbox bg-neutral-content">
-            <span class="grow">{i.name}{i.amount ? `, ${i.amount}` : ""}</span>
+    {#each recipe.ingredients as i (i.name)}
+        <div animate:flip={{duration: 1000, easing: cubicInOut, delay: 2000}}
+             class="flex justify-between gap-4">
+
+            <input type="checkbox"
+                   checked={$have.includes(i.name) ? "checked" : ""}
+                   on:click={() => onCheckChange(i.name)}
+                   class="checkbox bg-neutral-content">
+
+            <span class="grow"
+                  class:line-through={$have.includes(i.name) ? "checked" : ""}>
+                {i.name}{i.amount ? `, ${i.amount}` : ""}
+            </span>
         </div>
     {/each}
 </div>
