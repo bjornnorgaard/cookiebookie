8b4aab85bfdb394bd897da33f850344d82fcb9e2
Bjørn Nørgaard <bjornsnorgaard@gmail.com>
2023-01-12 17:05:57 +0100

    Added punctuation to footer message

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -14,7 +14,7 @@
 <footer class="pb-8 sm:px-8 bg-neutral-content">
     <div class="m-auto flex max-w-screen-sm flex-col justify-between gap-4 text-center sm:flex-row sm:px-8 sm:py-4 sm:text-left">
         <div>
-            <p>Recipes by Bear<br/>Providing unreliable cooking aid since 2023</p>
+            <p>Recipes by Bear<br/>Providing unreliable cooking aid since 2023.</p>
         </div>
         <div class="flex items-center justify-center gap-4 sm:justify-end">
             <a href="https://github.com/bjornnorgaard/cookieboogie">
