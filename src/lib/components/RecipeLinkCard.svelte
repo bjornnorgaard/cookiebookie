cc964fa64f6d4883e7068c7fedb49e78975fb5f0
Bjørn Nørgaard <bjornsnorgaard@gmail.com>
2023-01-13 16:47:31 +0100

    Updated formatting of RecipeLinkCard.svelte

--- src/lib/components/RecipeLinkCard.svelte
+++ src/lib/components/RecipeLinkCard.svelte
@@ -6,7 +6,10 @@
 </script>
 
 <a href={slug} class="overflow-hidden no-underline duration-500 ease-in-out card card-compact glass hover:scale-105">
-    <img class="m-0 rounded-t object-cover" height="300" width="700" src={`${recipe.image}?random=${recipe.slug}`} alt="car!"/>
+    <img class="m-0 rounded-t object-cover"
+         src={`${recipe.image}?random=${recipe.slug}`} alt="car!"
+         height="300" width="700"/>
+
     <div class="card-body prose">
         <h2 class="m-0 card-title">{recipe.title}</h2>
         <p>{recipe.shortDesc}</p>
