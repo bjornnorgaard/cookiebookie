e71e87c29a1588048581b760774723711fca4dc4
Bjørn Nørgaard <bjornsnorgaard@gmail.com>
2023-02-05 23:01:24 +0100

    Fixed typo in brod

--- src/recipes/brod.ts
+++ src/recipes/brod.ts
@@ -5,7 +5,7 @@ export const brod: Recipe = {
     title: "Uæltet brød",
     image: "",
     shortDesc: "Lækkert lyst, tæt og svampet brød, som ikke skal æltes. I hvert fald ikke særlig meget.",
-    longDesc: "Efter at lave konsulteret internettet lader dette til at være opskriften på sejr. Vi ælter ingenting, næsten, men tingene skal nu blandes alligevel - livet er hårdt. " +
+    longDesc: "Efter at have konsulteret internettet lader dette til at være opskriften på sejr. Vi ælter ingenting, næsten, men tingene skal nu blandes alligevel - livet er hårdt. " +
         "Når det skal bages, forvarmer jeg en stor støbejernsgryde ved max ovn. Alternativt kan et bagestål bruges. " +
         "Ellers kan den sikkert også ligge på en bageplade uden at gå i stykker - hvem ved.",
     ingredients: [
