79d4fb5a8ef9dd18dcb971174ee51802f77706e1
Bjørn Nørgaard <bjornsnorgaard@gmail.com>
2023-02-24 13:28:47 +0100

    Update sorens-pastaret.ts

--- src/recipes/sorens-pastaret.ts
+++ src/recipes/sorens-pastaret.ts
@@ -7,7 +7,7 @@ export const sorensPastaret: Recipe = {
     shortDesc: "Lav indsats, pisse lækker pastaret.",
     longDesc: "Nem ret. Smid det hele i et fad og bland. Kan sagtens klare " +
         "random grønsager - rosenkål og porre er ikke fjenden! " +
-        "Tager ingen tid at forbedrede og skal bare glemmes i ovnen i " +
+        "Tager ingen tid at forberede og skal bare glemmes i ovnen i " +
         "alt mellem 30 minutter og 24 timer - bliver nice.",
     ingredients: [
         {name: "Fyldt pasta", amount: "400g"},
@@ -32,7 +32,7 @@ export const sorensPastaret: Recipe = {
         },
         {
             title: "Ovnen klarer resten",
-            desc: "Det skal næppe have mindre end 30 minutter, men nok mere. " +
+            desc: "Det skal næppe have mindre end 30 minutter, lander normalt et sted omkring 45-60 min. " +
                 "Kig på den hver 20. minut og tænk dig om."
         },
         {
