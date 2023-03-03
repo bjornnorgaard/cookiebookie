37e4b0ed82b2d01c85a4ff8e8e7aa6456d9bf60b
Bjørn Nørgaard <bjornsnorgaard@gmail.com>
2023-03-03 03:08:29 +0100

    Removed redundant pruning in recipes.ts

--- src/lib/stores/recipes.ts
+++ src/lib/stores/recipes.ts
@@ -3,29 +3,7 @@ import { readable } from "svelte/store";
 import { all } from "../../recipes/_all";
 
 export const recipes = readable<Map<string, Recipe>>(new Map<string, Recipe>(), (set) => {
-    all.map(r => cleanRecipe(r))
     const list = new Map<string, Recipe>();
     all.forEach(r => list.set(r.slug, r))
     set(list)
 });
-
-function cleanRecipe(r: Recipe): Recipe {
-    if (!r.image.trim()) {
-        r.image = `https://picsum.photos/seed/${r.slug}/700/300`;
-    }
-    if (!r.slug) {
-        r.slug = createSlugFromTitle(r.title)
-    }
-    return r;
-}
-
-const createSlugFromTitle = (str: string) => {
-    str = str
-        .replace("a", "ae")
-        .replace("ø", "o")
-        .replace("å", "a");
-
-    let parsedStr = str.replace(/[^a-zA-Z0-9-_]/g, ' ').trim();
-    parsedStr = parsedStr.replace(/\s+/g, '-');
-    return parsedStr.toLowerCase();
-}
