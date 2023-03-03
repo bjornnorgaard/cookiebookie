a38d8144bc6729b8a0d77b56b5afdf4cba151014
Bjørn Nørgaard <bjornsnorgaard@gmail.com>
2023-03-03 03:03:27 +0100

    Added placeholder for boller.ts

---
+++ src/recipes/boller.ts
@@ -0,0 +1,15 @@
+import type { Recipe } from "$lib/types/recipe";
+
+export const boller: Recipe = {
+    slug: "boller",
+    title: "Fødselsdagsboller",
+    image: "https://picsum.photos/id/244",
+    shortDesc: "Kommer snart...",
+    longDesc: "Vi er der ikke helt endnu!",
+    ingredients: [
+        {name: "Mel", amount: "tjaa"},
+    ],
+    steps: [
+        {title: "Første skridt", desc: "Ingen ide"},
+    ],
+};
