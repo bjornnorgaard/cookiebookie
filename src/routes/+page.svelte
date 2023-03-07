00d8ee3d16ec909ab51eff41c5d141861982edbf
Bjørn Nørgaard <bjornsnorgaard@gmail.com>
2023-03-07 13:28:54 +0100

    Set main page recipe overview images to eager load

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -12,7 +12,7 @@
             <a href={slug}
                class="overflow-hidden no-underline duration-500 ease-in-out card card-compact glass hover:scale-105">
                 <img class="m-0 rounded-t object-cover"
-                     title={recipe.title} loading="lazy"
+                     title={recipe.title} loading="eager"
                      src={`${recipe.image}/700/300`} alt={recipe.title}
                      height="300" width="700"/>
 
