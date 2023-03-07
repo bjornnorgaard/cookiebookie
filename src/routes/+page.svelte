f79e264cc19da64037b48008996dd144bf6a3567
Bjørn Nørgaard <bjornsnorgaard@gmail.com>
2023-03-07 11:43:29 +0100

    Inlined and deleted RecipeLinkCard.svelte

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,16 +1,25 @@
 <script lang="ts">
-	import ContentNarrow from "$lib/components/ContentNarrow.svelte";
-	import RecipeLinkCard from "$lib/components/RecipeLinkCard.svelte";
-	import { recipes } from "$lib/stores/recipes.js";
-	import Head from "$lib/components/Head.svelte";
+    import ContentNarrow from "$lib/components/ContentNarrow.svelte";
+    import { recipes } from "$lib/stores/recipes.js";
+    import Head from "$lib/components/Head.svelte";
 </script>
 
 <Head/>
 
 <ContentNarrow title="Seneste opskrifter">
-	<div class="gap-4 grid-list">
-		{#each [...$recipes] as [slug, recipe]}
-			<RecipeLinkCard {slug} />
-		{/each}
-	</div>
+    <div class="gap-4 grid-list">
+        {#each [...$recipes] as [slug, recipe]}
+            <a href={slug}
+               class="overflow-hidden no-underline duration-500 ease-in-out card card-compact glass hover:scale-105">
+                <img class="m-0 rounded-t object-cover"
+                     src={`${recipe.image}/700/300`} alt={recipe.title}
+                     height="300" width="700"/>
+
+                <div class="card-body prose">
+                    <h2 class="m-0 card-title">{recipe.title}</h2>
+                    <p>{recipe.shortDesc}</p>
+                </div>
+            </a>
+        {/each}
+    </div>
 </ContentNarrow>
