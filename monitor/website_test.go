cd998456b3b28b506a042f209b894754f9f9dff4
Bjørn Nørgaard <bjornsnorgaard@gmail.com>
2023-03-12 14:21:01 +0100

    Wording update

--- monitor/website_test.go
+++ monitor/website_test.go
@@ -8,25 +8,20 @@ import (
 )
 
 func TestWebsite(t *testing.T) {
-	fmt.Println("starting test")
 	host := os.Getenv("SITE_HOST")
 	if len(host) == 0 {
 		fmt.Println("host was empty")
-		host = "http://localhost:5174"
 	}
 
-	fmt.Println("testing host", host)
+	fmt.Println("Checking host", host)
 
 	t.Run("should load main page", func(t *testing.T) {
 		response, err := http.Get(host)
 		if err != nil {
-			fmt.Println("http error", err)
 			t.Error(err)
 		}
 		if response.StatusCode != http.StatusOK {
-			fmt.Println("non 200")
 			t.Errorf("éxpected 200 received %d", response.StatusCode)
 		}
-		fmt.Println("done")
 	})
 }
