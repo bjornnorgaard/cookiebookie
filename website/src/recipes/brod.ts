31e910779f95d997596d877568febe5be30a624a
Bjørn Nørgaard <bjornsnorgaard@gmail.com>
2023-05-28 21:53:00 +0200

    Updated brod.ts

--- website/src/recipes/brod.ts
+++ website/src/recipes/brod.ts
@@ -1,27 +1,29 @@
-import type { Recipe } from "$lib/types/recipe";
-import { picsumHost } from "../lib/constants/picsum";
+import type {Recipe} from "$lib/types/recipe";
+import {picsumHost} from "../lib/constants/picsum";
 
 export const brod: Recipe = {
     slug: "brod",
     title: "Uæltet Brød",
     image: `${picsumHost}/id/40`,
     shortDesc: "Lækkert, tæt og svampet brød, som ikke skal æltes - særlig meget",
-    longDesc: "Efter at have konsulteret internettet lader dette til at være opskriften på sejr. Vi ælter ingenting, næsten, men tingene skal nu blandes alligevel - livet er hårdt. " +
-        "Når det skal bages, forvarmer jeg en stor støbejernsgryde ved max ovn. Alternativt kan et bagestål bruges. " +
-        "Ellers kan den sikkert også ligge på en bageplade uden at gå i stykker - hvem ved.",
     ingredients: [
         {name: "Hvedemel", amount: "400g"},
         {name: "Sukker", amount: "10g"},
         {name: "Salt", amount: "10g"},
-        {name: "Gær", amount: "1g"},
+        {name: "Tørgær", amount: "2g"},
         {name: "Vand", amount: "300g"},
     ],
+    longDesc: "Efter at have konsulteret internettet lader dette til at være opskriften på sejr. Vi ælter ingenting, næsten, men tingene skal nu blandes alligevel - livet er hårdt. " +
+        "Her finder du ingen mål i te- eller spiseskeer, som i konventionel madlavning. Bagning er alkemi og har intet at gøre med madlavning - så her bruger vi gram og liter! " +
+        "Når det skal bages, forvarmer jeg en stor støbejernsgryde ved max ovn. Alternativt kan et bagestål bruges. " +
+        "Ellers kan den sikkert også ligge på en bageplade uden at gå i stykker - hvem ved.",
     steps: [
-        {title: "Bland de våde sager", desc: "Der er ikke så meget andet at sige. Brug 10 sekunder og værdsæt at du næsten er færdig."},
-        {title: "Tilsæt de tørre sager", desc: "Bland det hele indtil det hænger sammen og der ikke meget mel på siderne af skålen. Hvis det tager dig mere end 40 sekunder prøver du for hårdt."},
+        {title: "Skål!", desc: "Find din største stål, som forhåbentligt har et låg - ellers bruger du et rent viskestykke"},
+        {title: "Bland de våde sager", desc: "Der er ikke så meget andet at sige. Gær, salt, sukker og tempereret vand. Brug 10 sekunder og værdsæt at du næsten er færdig."},
+        {title: "Tilsæt de tørre sager", desc: "Bland det hele indtil det hænger sammen og der ikke meget mel på siderne af skålen. Hvis det tager dig mere end to minutter prøver du for hårdt."},
         {title: "Hævetid er sovetid", desc: "Smid låg på. Det skal hvile i 12 til 24 timer, eller indtil det er fordoblet i størrelse. Tak for i dag - vi ses i morgen."},
         {title: "Den lever!", desc: "Vores baby er nu (forhåbentligt) vokset til mindst dobbelt størrelse og har pruttet hele natten - hvilket gerne skulle kunne duftes."},
-        {title: "Bagetid", desc: "Du tænder ovnen på alt hvad den trække og smider din støbejernsfætter ind. Vi mødes her igen om ~45 min."},
+        {title: "Bagetid", desc: "Du tænder ovnen på alt hvad den trække og forvarmer din støbejernsfætter. Vi mødes her igen om ~45 min."},
         {title: "Nu kommer svineriet", desc: "Hør nu efter. Det bliver et klisterhelvede. Der skal masser af mel på bordet og vand på fingerne - i den rækkefølge! Den skal ikke æltes, men bare lige foldes nogle gange. Gør det nogle få gange indtil den ligner en pæn dej med en stram overflade."},
         {title: "Brænd nu ikke dine små dumme fingre", desc: "Smid den på et stykke velmelet bagepapir og sprøjt den gavmildt til med vand. Sænk den så forsigtigt ned i din støbejernsgryde (pas nu på pølserne). Sæt låg på og efterlad i ovnen i 20 minutter før næste inspektion."},
         {title: "Tjekketid", desc: "Kig til brødet. Hvis den ikke er færdig, sættes du den tilbage i ovnen. Hvis du er i tvivl spørger du en voksen."},
