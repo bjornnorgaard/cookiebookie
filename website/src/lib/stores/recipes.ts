919451e1c883dc7768cafa523cc3ee3be9ab3e4a
Bjørn Nørgaard <bjornsnorgaard@gmail.com>
2023-08-14 13:00:10 +0200

    Deleted all recipes

--- website/src/lib/stores/recipes.ts
+++ website/src/lib/stores/recipes.ts
@@ -1,20 +1,3 @@
-import type {Recipe} from "$lib/types/recipe";
-import { brod } from "../../recipes/brod";
-import { lasagne } from "../../recipes/lasagne";
-import { risotto } from "../../recipes/risotto";
-import { ristaffel } from "../../recipes/ristaffel";
-import { sorensPastaret } from "../../recipes/sorens-pastaret";
-import { svinemorbrad } from "../../recipes/spansk-svinemorbrad";
-import { tomatsalat } from "../../recipes/tomatsalat";
+import type { Recipe } from "$lib/types/recipe";
 
-export const recipes: Recipe[] = [
-    ristaffel,
-    lasagne,
-    sorensPastaret,
-    risotto,
-    brod,
-    tomatsalat,
-    svinemorbrad,
-    // boller,
-    // frittata,
-];
+export const recipes: Recipe[] = [];
