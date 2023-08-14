c7ea53b133e9d5ba6cb9f6fedb0a3ac0264440f0
Bjørn Nørgaard <bjornsnorgaard@gmail.com>
2023-08-14 11:42:39 +0200

    Fixed CSP policy with nonce for gtag

--- website/svelte.config.js
+++ website/svelte.config.js
@@ -11,7 +11,7 @@ const config = {
         csp: {
             mode: "auto",
             directives: {
-                "script-src": ["self", "unsafe-inline"],
+                "script-src": ["self"],
                 "object-src": ["none"],
                 "base-uri": ["self"],
             }
