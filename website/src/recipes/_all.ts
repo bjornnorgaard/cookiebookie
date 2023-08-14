fbdb913635c6ac1761b438b54eccd12b8fce3110
Bjørn Nørgaard <bjornsnorgaard@gmail.com>
2023-08-14 09:37:30 +0200

    Removed redundant newline in _all.ts

--- website/src/recipes/_all.ts
+++ website/src/recipes/_all.ts
@@ -4,9 +4,8 @@ import {brod} from "./brod";
 import {ristaffel} from "./ristaffel";
 import type {Recipe} from "$lib/types/recipe";
 import {sorensPastaret} from "./sorens-pastaret";
-import { risotto } from "./risotto";
-import { svinemorbrad } from "./spansk-svinemorbrad";
-
+import {risotto} from "./risotto";
+import {svinemorbrad} from "./spansk-svinemorbrad";
 
 // Recipes will appear in the listed order.
 export const all: Recipe[] = [
