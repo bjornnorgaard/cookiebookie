47587cf0c57d10454da34165c894772d29e79c9c
Bjørn Nørgaard <bjornsnorgaard@gmail.com>
2023-08-15 15:36:15 +0200

    custom font and other styling updates (#308)

--- website/src/lib/components/MainContent.svelte
+++ website/src/lib/components/MainContent.svelte
@@ -3,7 +3,7 @@
     export let title = "Missing title";
 </script>
 
-<main class="m-auto max-w-screen-sm px-4 min-h-content prose py-nav">
-    <h1>{title}</h1>
+<div class="m-auto max-w-screen-sm px-4 min-h-content prose py-nav">
+    <h1 class="text-base-content tracking-wide">{title}</h1>
     <slot/>
-</main>
+</div>
--- website/src/routes/[slug]/+page.svelte
+++ website/src/routes/[slug]/+page.svelte
@@ -16,31 +16,50 @@
          title={data.recipe.title} loading="eager"
          src={`${data.recipe.image}/1260/540`} alt={data.recipe.title}
          height="300" width="700">
-    <p class="m-0">{data.recipe.longDesc}</p>
 
-    <ShoppingList recipe={data.recipe}/>
+    <p class="text-base-content">{data.recipe.longDesc}</p>
 
-    <h2>Planen</h2>
-    <div class="flex flex-col gap-4 rounded-lg p-4 drop-shadow-lg bg-secondary text-secondary-content">
-        {#each data.recipe.steps as s,id}
-            <div class="flex gap-4">
-                <input id={s.title} type="checkbox" bind:checked={completedSteps[id]}
-                       class="mt-1 checkbox bg-neutral-content text-neutral-content">
-                <div class="flex flex-col">
-                    <label for={s.title}
-                           class="mt-1 text-base font-bold transition-opacity"
-                           class:line-through={completedSteps[id]}
-                           class:opacity-50={completedSteps[id]}>
-                        {s.title}
-                    </label>
+    <section>
+        <div class="relative">
+            <h2>Indkøbslisten</h2>
+            <div class="bg-primary absolute bottom-0 left-0 h-1 w-10 rounded-2xl"></div>
+        </div>
 
-                    <p class="m-0 transition-opacity"
-                       class:line-through={completedSteps[id]}
-                       class:opacity-50={completedSteps[id]}>
-                        {s.desc}
-                    </p>
-                </div>
+        <div class="bg-primary text-primary-content rounded-lg drop-shadow-lg card">
+            <ShoppingList recipe={data.recipe}/>
+        </div>
+    </section>
+
+    <section>
+        <div class="relative">
+            <h2>Planen</h2>
+            <div class="bg-secondary absolute bottom-0 left-0 h-1 w-10 rounded-2xl"></div>
+        </div>
+
+        <div class="bg-secondary text-secondary-content rounded-lg drop-shadow-lg card">
+            <div class="flex flex-col gap-4 p-4">
+                {#each data.recipe.steps as s,id}
+                    <div class="flex gap-4">
+                        <input id={s.title} type="checkbox" bind:checked={completedSteps[id]}
+                               class="mt-1 checkbox bg-neutral-content text-neutral-content">
+                        <div class="flex flex-col">
+                            <label for={s.title}
+                                   class="mt-1 w-fit cursor-pointer text-base font-bold transition-opacity hover:line-through"
+                                   class:line-through={completedSteps[id]}
+                                   class:opacity-50={completedSteps[id]}>
+                                {s.title}
+                            </label>
+
+                            <p class="m-0 transition-opacity"
+                               class:line-through={completedSteps[id]}
+                               class:opacity-50={completedSteps[id]}>
+                                {s.desc}
+                            </p>
+                        </div>
+                    </div>
+                {/each}
             </div>
-        {/each}
-    </div>
+        </div>
+    </section>
+
 </MainContent>
