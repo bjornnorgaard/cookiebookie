946d309dd68f409c89d4fbf2ed2a650c0342b542
Bjørn Nørgaard <bjornsnorgaard@gmail.com>
2025-08-29 12:19:08 +0200

    Refactor +page.svelte: update styles for consistency, simplify components, and enhance layout structure

--- src/routes/[slug]/+page.svelte
+++ src/routes/[slug]/+page.svelte
@@ -2,7 +2,6 @@
     import Head from "$lib/components/Head.svelte";
     import ShoppingList from "$lib/components/ShoppingList.svelte";
     import MainContent from "$lib/components/MainContent.svelte";
-    import { page } from "$app/stores";
 
     let {data} = $props();
     const completedSteps: boolean[] = $state([]);
@@ -16,49 +15,45 @@
          src={`${data.recipe.image}/1260/540`} alt={data.recipe.title}
          height="300" width="700">
 
-    <p class="text-base-content">{data.recipe.longDesc}</p>
+    <p class="py-4">{data.recipe.longDesc}</p>
 
     <section>
         <div class="relative">
-            <h2>Indkøbslisten</h2>
-            <div class="absolute bottom-0 left-0 h-1 w-10 rounded-2xl"></div>
+            <h4 class="h4">Indkøbslisten</h4>
+            <div class="absolute bottom-0 bg-primary-500 left-0 h-1 w-10 rounded-2xl"></div>
         </div>
 
-        <div class="rounded-lg drop-shadow-lg">
+        <div class="preset-tonal-primary card my-4">
             <ShoppingList recipe={data.recipe}/>
         </div>
     </section>
 
     <section>
         <div class="relative">
-            <h2>Planen</h2>
-            <div class="absolute bottom-0 left-0 h-1 w-10 rounded-2xl"></div>
+            <h4 class="h4">Planen</h4>
+            <div class="absolute bottom-0 bg-secondary-500 left-0 h-1 w-10 rounded-2xl"></div>
         </div>
 
-        <div class="rounded-lg drop-shadow-lg">
-            <div class="flex flex-col gap-4 p-4">
-                {#each data.recipe.steps as s, i}
-                    <div id={"step" + (i+1)} class="flex gap-4"
-                         class:bg-yellow-400={$page.url.toString().includes("#step" + (i+1))}>
-                        <input id={s.title} type="checkbox" bind:checked={completedSteps[i]}
-                               class="mt-1">
-                        <div class="flex flex-col">
-                            <label for={s.title}
-                                   class="mt-1 w-fit cursor-pointer font-bold transition-opacity hover:line-through"
-                                   class:line-through={completedSteps[i]}
-                                   class:opacity-50={completedSteps[i]}>
-                                {s.title}
-                            </label>
-
-                            <p class="m-0 transition-opacity"
+        <div class="card preset-tonal-secondary my-4 flex flex-col gap-4 p-4">
+            {#each data.recipe.steps as s, i}
+                <div id={"step" + (i+1)} class="flex gap-4">
+                    <input id={s.title} type="checkbox" bind:checked={completedSteps[i]} class="mt-1">
+                    <div class="flex flex-col">
+                        <label for={s.title}
+                               class="mt-1 w-fit cursor-pointer font-bold transition-opacity hover:line-through"
                                class:line-through={completedSteps[i]}
                                class:opacity-50={completedSteps[i]}>
-                                {s.desc}
-                            </p>
-                        </div>
+                            {s.title}
+                        </label>
+
+                        <p class="m-0 transition-opacity"
+                           class:line-through={completedSteps[i]}
+                           class:opacity-50={completedSteps[i]}>
+                            {s.desc}
+                        </p>
                     </div>
-                {/each}
-            </div>
+                </div>
+            {/each}
         </div>
     </section>
 
