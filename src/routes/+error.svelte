4ce65e7ce43402b0f618f452bea6819ca313efa8
Bjørn Nørgaard <bjornsnorgaard@gmail.com>
2025-12-29 23:00:38 +0100

    Refactor +error.svelte: improve layout structure, enhance button styling, and adjust spacing for consistency

--- src/routes/+error.svelte
+++ src/routes/+error.svelte
@@ -1,19 +1,21 @@
 <script>
-    import { picsumHost } from "$lib/constants/picsum";
+    import {picsumHost} from "$lib/constants/picsum";
     import MainContent from "$lib/components/MainContent.svelte";
 </script>
 
-<img class="rounded-lg drop-shadow-lg"
-     src={`${picsumHost}/id/404//1400/600`} alt="havet der støder mod store klipper"
-     height="300" width="700">
-
 <MainContent title="Du er vist faret vild">
-    <div class="flex flex-col items-center text-center">
+    <img class="rounded-lg drop-shadow-lg"
+         src={`${picsumHost}/id/404//1400/600`}
+         alt="havet der støder mod store klipper"
+         height="300"
+         width="700">
+
+    <div class="flex flex-col py-4 gap-4 items-center text-center">
         <h3 class="m-0">Det skal nok gå</h3>
 
         <p>Jeg har naturligvis forudset, at du ville gå ud hvor du ikke kunne bunde.
-           Har endda lavet en lille knap til dig. Så kan du komme tilbage i sikkerhed.</p>
+            Har endda lavet en lille knap til dig. Så kan du komme tilbage i sikkerhed.</p>
 
-        <a href="/" class="no-underline">Gå til Forsiden</a>
+        <a href="/" class="no-underline btn btn-lg preset-filled-primary-500">Gå til Forsiden</a>
     </div>
 </MainContent>
