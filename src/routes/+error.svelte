11b972e6b82c303fc35bbec13e6c18aa3acc25b9
Bjørn Nørgaard <bjornsnorgaard@gmail.com>
2025-12-30 00:06:58 +0100

    Refactor +error.svelte: adjust button styling, add period to heading for consistency

--- src/routes/+error.svelte
+++ src/routes/+error.svelte
@@ -11,11 +11,11 @@
          width="700">
 
     <div class="flex flex-col py-4 gap-4 items-center text-center">
-        <h3 class="m-0">Det skal nok gå</h3>
+        <h3 class="m-0">Det skal nok gå.</h3>
 
         <p>Jeg har naturligvis forudset, at du ville gå ud hvor du ikke kunne bunde.
             Har endda lavet en lille knap til dig. Så kan du komme tilbage i sikkerhed.</p>
 
-        <a href="/" class="no-underline btn btn-lg preset-filled-primary-500">Gå til Forsiden</a>
+        <a href="/" class="no-underline btn my-4 preset-filled-primary-500">Gå til Forsiden</a>
     </div>
 </MainContent>
